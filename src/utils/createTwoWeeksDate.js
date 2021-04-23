a3d07b6620dc89324904fbdb7db259a1202f2246
Iqdam Musayyad <iamusayyad@gmail.com>
2021-04-23 23:28:58 +0700

    function to generate 2 weeks date starting from mon

---
+++ src/utils/createTwoWeeksDate.js
@@ -0,0 +1,27 @@
+export default function createTwoWeeksDate() {
+  const currentDate = new Date();
+
+  let diff =
+    currentDate.getDate() -
+    currentDate.getDay() +
+    (currentDate.getDay() === 0 ? -6 : 1);
+
+  const startOfWeek = new Date(currentDate.setDate(diff));
+
+  const year = startOfWeek.getFullYear();
+  const month = startOfWeek.getMonth();
+  const date = startOfWeek.getDate();
+
+  const dateList = [startOfWeek];
+
+  for (let i = 1; i <= 6; i++) {
+    const nextDate = new Date(year, month, date + i, 23, 59, 59);
+    dateList.push(nextDate);
+    // console.log(`next ${i} day`);
+    // console.log(nextDate.toDateString());
+    // console.log(nextDate.getDay());
+    // console.log(nextDate.getDate());
+  }
+
+  return dateList;
+}
