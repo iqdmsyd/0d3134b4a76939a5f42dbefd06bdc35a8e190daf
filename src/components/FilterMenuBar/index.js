6a272ad499343e1c97d0ef9290ca9d801ab9bb96
Iqdam Musayyad <iamusayyad@gmail.com>
2021-04-23 22:38:44 +0700

    fix scroll behaviour on desktop and mobile

--- src/components/FilterMenuBar/index.js
+++ src/components/FilterMenuBar/index.js
@@ -7,36 +7,49 @@ const FilterMenuBar = (props) => {
   const [open, setOpen] = useState(true);
   const dateList = createTwoWeeksDate();
 
-  let lastScrollTop = 0;
+  // let lastScrollTop = 0;
 
   // function checkScroll(e) {
   //   var st = window.pageYOffset; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-  //   console.log(st);
   //   if (st > lastScrollTop) {
   //     // downscroll code
   //     setOpen(false);
-  //   } else {
+  //   } else if (st < lastScrollTop) {
   //     // upscroll code
   //     setOpen(true);
   //   }
   //   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
   // }
 
-  // window.addEventListener("scroll", checkScroll);
+  // window.addEventListener("scroll", checkScroll, false);
 
+  // work in browser
   window.addEventListener(
     "wheel",
     (e) => {
       if (e.deltaY > 0) {
-        console.log("down");
         setOpen(false);
       } else {
-        console.log("up");
         setOpen(true);
       }
     },
     false
   );
+
+  // work in mobile
+  let ts;
+  document.addEventListener("touchstart", (e) => {
+    ts = e.targetTouches[0].clientY;
+  });
+
+  document.addEventListener("touchmove", (e) => {
+    let te = e.changedTouches[0].clientY;
+    if (ts > te) {
+      setOpen(false);
+    } else if (ts < te) {
+      setOpen(true);
+    }
+  });
 
   return React.createElement(FilterMenuBarView, { dateList, open });
 };
