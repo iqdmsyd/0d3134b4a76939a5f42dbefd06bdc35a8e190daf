7c6a26c6e18b134472d5a174e8cfe12f48252cef
Iqdam Musayyad <iamusayyad@gmail.com>
2021-04-24 11:42:57 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import AppBar from "./components/AppBar";
+import { useState } from "react";
+
 import Container from "./components/Container";
-import FoodCard from "./components/FoodCard";
-import HeaderText from "./components/HeaderText";
+import AppBar from "./components/AppBar";
+import FilterMenuBar from "./components/FilterMenuBar";
+import LocationModal from "./components/LocationModal";
+import ShoppingCart from "./components/ShoppingCart";
+import FoodCardList from "./components/FoodCardList";
 
 const FOODS = [
   {
@@ -11,27 +15,67 @@ const FOODS = [
   },
   {
     name: "Roasted Chicken with Scrambled Egg",
-    rating: 4.5,
+    rating: 3.5,
     price: 35_000,
   },
   {
     name: "Roasted Chicken with Scrambled Egg",
-    rating: 4.5,
+    rating: 2,
     price: 35_000,
   },
 ];
 
 function App() {
+  const [openModal, setOpenModal] = useState(false);
+  const [location, setLocation] = useState("Kulina");
+  const [dateSelected, setDateSelected] = useState(new Date());
+  const [cartItems, setCartItems] = useState([]);
+
+  const toggleModal = () => {
+    setOpenModal((prev) => !prev);
+  };
+
+  const changeLocation = (loc) => {
+    setLocation(loc);
+  };
+
+  const addItems = (item) => {
+    setCartItems((prev) => [...prev, item]);
+  };
+
+  const changeDateSelected = (date) => {
+    setDateSelected(date);
+  };
+
   return (
     <div className="App">
       <Container>
-        <AppBar location="Tokopedia Tower" />
-        <div style={{ width: "100%", height: "50px" }}></div>
-        <HeaderText size={14}>Kamis, 22 April 2021</HeaderText>
-        {FOODS.map((food, idx) => (
-          <FoodCard key={idx} food={food} />
-        ))}
+        {/* Appbar */}
+        <AppBar location={location} handleOpenModal={toggleModal} />
+        <div style={{ width: "100%", height: "138px" }}></div>
+
+        {/* FilterMenuBar */}
+        <FilterMenuBar date={dateSelected} changeDate={changeDateSelected} />
+
+        {/* FoodCard List */}
+        <FoodCardList
+          items={FOODS}
+          handleAddItem={addItems}
+          date={dateSelected}
+        />
+
+        {/* Fixed Cart */}
+        <ShoppingCart items={cartItems} />
+        {/* prevent overlap between shopping cart and food card */}
+        {cartItems.length > 0 && (
+          <div style={{ width: "100%", height: "50px" }}></div>
+        )}
       </Container>
+      <LocationModal
+        open={openModal}
+        handleCloseModal={toggleModal}
+        changeLocation={changeLocation}
+      />
     </div>
   );
 }
--- src/utils/createTwoWeeksDate.js
+++ src/utils/createTwoWeeksDate.js
@@ -1,12 +1,13 @@
 export default function createTwoWeeksDate() {
+  // start from monday
   const currentDate = new Date();
-
   let diff =
     currentDate.getDate() -
     currentDate.getDay() +
     (currentDate.getDay() === 0 ? -6 : 1);
 
   const startOfWeek = new Date(currentDate.setDate(diff));
+  // const startOfWeek = new Date();
 
   const year = startOfWeek.getFullYear();
   const month = startOfWeek.getMonth();
@@ -14,7 +15,7 @@ export default function createTwoWeeksDate() {
 
   const dateList = [startOfWeek];
 
-  for (let i = 1; i <= 6; i++) {
+  for (let i = 1; i <= 13; i++) {
     const nextDate = new Date(year, month, date + i, 23, 59, 59);
     dateList.push(nextDate);
     // console.log(`next ${i} day`);
